2be1a5e269064ddda645a9b8deaa4a6b0ce28dce
OliverNorborg <olivernorborg@Olivers-MacBook-Pro.local>
2022-09-02 17:29:10 +0200

    Week 2

--- Week2/Exercise_1b.py
+++ Week2/Exercise_1b.py
@@ -6,10 +6,18 @@ Created on Tue Aug 30 15:53:52 2022
 @author: olivernorborg
 """
 
+"""
+Help with exercises:
+Exercise 4 - Clarification fo covarience and the plot
+Exercise 6 - Have i found the right covariance in exercise 5?
+
+"""
+
 import numpy as np
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import decomposition
 
 in_dir = "data/"
 txt_name = "irisdata.txt"
@@ -54,10 +62,86 @@ print("Covariance for the width between sepal and petal is: ", Cov_width)
 
 """Exercise 4 - """
 
-plt.figure() # Added this to make sure that the figure appear
-# Transform the data into a Pandas dataframe
-d = pd.DataFrame(x, columns=['Sepal length', 'Sepal width','Petal length', 'Petal width'])
-sns.pairplot(d)
+# plt.figure() # Added this to make sure that the figure appear
+# # Transform the data into a Pandas dataframe
+# d = pd.DataFrame(x, columns=['Sepal length', 'Sepal width','Petal length', 'Petal width'])
+# sns.pairplot(d)
+
+# #Saves the image -uncomment to save
+# #plt.savefig('pairplot.png')
+# plt.show()
+
+# What measurements are related and which ones are not-related? 
+#   Sepal elngth a width seem to have a linear correlation
+#Can you recognise the results you found, when you computed the variance and covariance?
+#   Not really...?
+
+"""Exercise 5 - PCA"""
+#Subtracting the mean from the data
+mn = np.mean(x, axis=0)
+data = x - mn
+
+sep_l_data = data[:, 0]
+sep_w_data = data[:, 1]
+pet_l_data = data[:, 2]
+pet_w_data = data[:, 3]
+
+C_sep = (1/(n_obs-1))*np.matmul(sep_l_data, np.transpose(sep_w_data))
+C_sep_l = (1/(n_obs-1))*np.matmul(sep_l_data, np.transpose(sep_l_data))
+C_sep_w = (1/(n_obs-1))*np.matmul(sep_w_data, np.transpose(sep_w_data))
+C_pet = (1/(n_obs-1))*np.matmul(pet_l_data, np.transpose(pet_w_data))
+C_pet_l = (1/(n_obs-1))*np.matmul(pet_l_data, np.transpose(pet_l_data))
+C_pet_w = (1/(n_obs-1))*np.matmul(pet_w_data, np.transpose(pet_w_data))
+print("C_Sep:",C_sep)
+print("C_sep_l:", C_sep_l)
+print("C_sep_w:", C_sep_w)
+C_sep_matrix = np.array([[C_sep_l,C_sep],[C_sep, C_sep_w]])
+print("Own result:")
+print(C_sep_matrix)
+print("np.cov result:")
+print(np.cov(sep_l_data,sep_w_data))
+
+
+"""Exercise 6 - Principle componets"""
+#eig(covaraince matrix), which covaraince matrix?
+#values, vectors = np.linalg.eig(c_x)
+
+"""Exercise 7 - """
+#Values are found in the above exercise
+v_norm = values / values.sum() * 100
+plt.plot(v_norm)
+plt.xlabel('Principal component')
+plt.ylabel('Percent explained variance')
+plt.ylim([0, 100])
 plt.show()
+
+"""Exercise 8 - Projecting data onto PCA space"""
+pc_proj = vectors.T.dot(data.T)
+
+"""Exercise 9 - A different way to find PCA"""
+pca = decomposition.PCA()
+pca.fit(x)
+values_pca = pca.explained_variance_
+exp_var_ratio = pca.explained_variance_ratio_
+vectors_pca = pca.components_
+data_transform = pca.transform(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
